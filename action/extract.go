8862cc329418490a4a97933a0f42852787d3cceb
James Lawson <consulting@caelisco.net>
2023-10-18 19:47:39 +1100

    Fixed positioning of strings.Contains in extract.

--- action/extract.go
+++ action/extract.go
@@ -10,12 +10,11 @@ import (
 
 func ExtractTGZ(file string) error {
 	fmt.Println("file:", file)
-	if !strings.Contains(".tgz", file) {
+	if !strings.Contains(file, ".tgz") {
 		return fmt.Errorf("unknown extension, cannot extract from %s", file)
 	}
 	if _, err := os.Stat(file); os.IsNotExist(err) {
-		fmt.Println("input file does not exist:", file)
-		return err
+		return fmt.Errorf("input file does not exist: %s", file)
 	}
 	fmt.Print("extracting ", file)
 	_, err := exec.RunOutput("tar", "-zxvf", file)
