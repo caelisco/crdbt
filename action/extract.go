75421db30b03636cff1debfac077b645d1e812bc
James Lawson <consulting@caelisco.net>
2023-10-18 20:48:43 +1100

    Minor fixes to extract

--- action/extract.go
+++ action/extract.go
@@ -9,14 +9,13 @@ import (
 )
 
 func ExtractTGZ(file string) error {
-	fmt.Println("file:", file)
 	if !strings.Contains(file, ".tgz") {
 		return fmt.Errorf("unknown extension, cannot extract from %s", file)
 	}
 	if _, err := os.Stat(file); os.IsNotExist(err) {
 		return fmt.Errorf("input file does not exist: %s", file)
 	}
-	fmt.Print("extracting ", file)
+	fmt.Print("extracting", file)
 	_, err := exec.RunOutput("tar", "-zxvf", file)
 	if err != nil {
 		return err
